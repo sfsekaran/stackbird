383568903701a30409f29424d26f1d24d08c8003
Sathya Sekaran <sfsekaran@gmail.com>
2017-03-25 19:11:16 -0700

    add "clearance" authorization gem

--- Gemfile
+++ Gemfile
@@ -33,6 +33,9 @@ gem 'jbuilder', '~> 2.5'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
+# clearence for user auth
+gem 'clearance'
+
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170326020522) do
+ActiveRecord::Schema.define(version: 20170326020942) do
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "email"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 20170326020522) do
     t.string   "confirmation_token"
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
+    t.index ["email"], name: "index_users_on_email", using: :btree
+    t.index ["remember_token"], name: "index_users_on_remember_token", using: :btree
   end
 
 end
