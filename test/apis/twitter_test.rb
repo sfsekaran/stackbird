ab099ef97c05600927bfaf7d95233acd789556d9
Sathya Sekaran <sfsekaran@gmail.com>
2017-03-25 22:01:06 -0700

    add simple tests for Twitter api class

---
+++ test/apis/twitter_test.rb
@@ -0,0 +1,16 @@
+require 'test_helper'
+
+class TwitterTest < ActiveSupport::TestCase
+
+  test "initialization" do
+    assert Twitter.new
+  end
+
+  # TODO: we should cache the API call using VCR or similar so we don't hit Twitter every time we run this test.
+  test "it gets the last 25 tweets for a user" do
+    t = Twitter.new
+    tweets = t.timeline('support')
+    assert tweets.count == 25
+  end
+
+end
